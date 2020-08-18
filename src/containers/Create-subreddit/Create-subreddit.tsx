e036a8f8e48ee2883a87b9357e7fc6c4578d088d
bowen <447099673@qq.com>
2020-08-18 22:33:15 +1000

    Create subreddit added

--- src/containers/Create-subreddit/Create-subreddit.tsx
+++ src/containers/Create-subreddit/Create-subreddit.tsx
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Create-subreddit.css'
+import {createSubReddit} from '../../services/subreddit.service';
+import {useDispatch} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+
 
 const CreateSubReddit: React.FC = () => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    description: ''
+  });
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [submitted, setSubmitted] = useState(false);
+
+  const {name, description} = inputs;
+
+  function handleChange(e: any) {
+    const {name, value} = e.target;
+    setInputs(inputs => ({...inputs, [name]: value}));
+  }
+
+  function discard() {
+    setInputs({
+      name: '',
+      description: ''
+    });
+    setSubmitted(false);
+  }
+
+  function submit(e: any) {
+    e.preventDefault();
+    setSubmitted(true);
+
+    if (name && description) {
+      createSubReddit(dispatch, {name, description}).then(
+        (resp) => {
+          history.push('/home');
+        }
+      )
+    }
+  }
+
   return (
-    <div>Create Subreddit</div>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="create-subreddit-container">
+          <form className="post-form">
+            <div className="form-group">
+              <div className="create-subreddit-heading">Create Subreddit</div>
+            </div>
+            <hr/>
+
+            <input type="text" name="name" className="form-control mt-2" placeholder="Title" value={name} onChange={handleChange} required/>
+            {submitted && !name && <span className="text-danger">Title is required</span>}
+
+            <textarea name="description" className="w-100 mt-2" placeholder="Description" value={description} onChange={handleChange} required/>
+            {submitted && !description && <span className="text-danger">Description is required</span>}
+
+            <div>
+              <div className="mt-2 float-right">
+                <button onClick={discard} type="button" className="btnDiscard">Discard</button>
+                <button onClick={submit}  type="button" className="btnCreateSubreddit">Create</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/services/subreddit.service.ts
+++ src/services/subreddit.service.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import {clearAlert, errorAlert, successAlert} from '../actions/alert.action';
+import {Dispatch} from 'react';
 const base_url = 'http://localhost:8080/api';
 
 export const getAllSubreddits = (): Promise<any> => {
@@ -7,6 +9,24 @@ export const getAllSubreddits = (): Promise<any> => {
       console.log(error);
     });
 };
+
+export const createSubReddit = (dispatch: Dispatch<any>, subreddit: ISubreddit): Promise<any> => {
+  return axios.post(`${base_url}/subreddit/`, subreddit)
+    .then((response) => {
+      dispatch(successAlert(`Subreddit ${subreddit.name} successfully created`));
+      setTimeout(() => {
+        dispatch(clearAlert(''))
+      }, 3000)
+
+    })
+    .catch((error) => {
+      dispatch(errorAlert(error.message));
+      setTimeout(() => {
+        dispatch(clearAlert(''))
+      }, 3000)
+      console.log(error);
+    });
+}
 
 export interface ISubreddit {
   id?: number;
